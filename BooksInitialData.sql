79e9115fd968cab0079b3901686d72e2b5bb8a57
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-03-06 15:10:45 +0000

    Added sql data for books table

---
+++ BooksInitialData.sql
@@ -0,0 +1,8 @@
+INSERT INTO "Book" ("Title", "Author")
+VALUES
+ 	('An Hobbit', 'J. R. R. Tolkien'),
+ 	('Watership Down', 'Richard Adam'),
+	('Gruffalo', 'Julia Donaldson'),
+	('Once and Future King', 'T. H. White'),
+	('Stick Man', 'Julia Donaldson'),
+	('Redwall', 'Brian Jacques');
