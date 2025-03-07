4316f69f773e7a90b424393f2c67d2500373b466
S Chiang <63751262+gosaurus@users.noreply.github.com>
2025-03-07 11:23:46 +0000

    Added EditBook form/view and routing

--- Controllers/BookishController.cs
+++ Controllers/BookishController.cs
@@ -26,6 +26,43 @@ public class BookishController : Controller
         return View();
     }
 
+    // GET: Bookish/Edit/<int: id>
+    public async Task<IActionResult> EditBook(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        
+        var book = await _context.Book.FindAsync(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        return View(book);
+    }
+    
+    // POST: Bookish/Edit/<int: id>
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditBook(int id, [Bind("Id,Title,Author")] Book book)
+    {
+        if (id != book.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            _context.Update(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(book);
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
